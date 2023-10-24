6822b5e1e026ec982d2cbb5d4cdbdbd0d6b76cf6
Anna Dubik <anngwork@gmail.com>
2023-10-24 13:14:44 +0300

    Fix common logic move out from brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,27 +1,18 @@
 #!/usr/bin/env node
-import getRangeRandomNumber from '../../bin/utils.js';
-import finishTask, {
-  greet, writeTask, getReply, checkAnswer,
-} from '../../bin/common.js';
+import getRangeRandomNumber, { roundsCount } from '../../bin/utils.js';
+import runGame from '../../bin/common.js';
 
-const mathCalculator = (num1, num2, name) => {
-  // Create random sign for calculator
-  const signs = ['+', '-', '*'];
-  const randomIndex = Math.round(Math.random() * (signs.length - 1));
-  const randomSign = signs[randomIndex];
+const mathCalculator = (num1, num2, randomSign, name) => {
   let result = 0;
 
   switch (randomSign) {
     case '+':
-      console.log(`Question: ${num1} + ${num2}`);
       result = num1 + num2;
       break;
     case '-':
-      console.log(`Question: ${num1} - ${num2}`);
       result = num1 - num2;
       break;
     case '*':
-      console.log(`Question: ${num1} * ${num2}`);
       result = num1 * num2;
       break;
     default:
@@ -32,28 +23,25 @@ const mathCalculator = (num1, num2, name) => {
 };
 
 function runBrainCalc() {
-  const name = greet();
-  let correctAnswers = 0;
+  const textTask = 'What is the result of the expression?';
+  const questionArr = [];
+  const correctAnswerArr = [];
 
-  writeTask('What is the result of the expression?');
-
-  while (correctAnswers < 3) {
-    // Create two random numbers for calculator
+  for (let i = 0; i < roundsCount; i += 1) {
     const num1 = getRangeRandomNumber(0, 100);
     const num2 = getRangeRandomNumber(0, 100);
 
-    const correctAnswer = mathCalculator(num1, num2, name);
-    const reply = getReply();
-    checkAnswer(correctAnswer, reply, name, correctAnswers);
+    // Create random sign for calculator
+    const signs = ['+', '-', '*'];
+    const randomIndex = Math.round(Math.random() * (signs.length - 1));
+    const randomSign = signs[randomIndex];
 
-    if (correctAnswer === Number(reply)) {
-      correctAnswers += 1;
-    } else {
-      break;
-    }
+    questionArr.push(`${num1} ${randomSign} ${num2}`);
+    const correctAnswer = mathCalculator(num1, num2, randomSign);
+    correctAnswerArr.push(correctAnswer);
   }
 
-  finishTask(correctAnswers, name);
+  runGame(textTask, questionArr, correctAnswerArr);
 }
 
 export default runBrainCalc;
