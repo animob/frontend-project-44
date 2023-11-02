1dc438fe9cd2c32c275ae059ee0c6559304f8bbf
Anna Dubik <anngwork@gmail.com>
2023-11-02 23:02:26 +0300

    Fix brain-prime game (numbers less than 2 and limit)

--- src/games/runPrime.js
+++ src/games/runPrime.js
@@ -4,11 +4,18 @@ import runGame from '../index.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+  const limit = Math.sqrt(num);
+
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= limit; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
+
   return true;
 };
 
